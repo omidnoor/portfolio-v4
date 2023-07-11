b7d594fc6c61e2d64956f6079fc86f0ebbdf0e7c
Omid Noorshams <o.noorshams@gmail.com>
2023-07-10 19:59:12 -0700

    update contact me component

---
+++ src/components/3d/TitlePlate.jsx
@@ -0,0 +1,15 @@
+import { useMatcapTexture } from "@react-three/drei";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const TitlePlate = ({ props, setTitle }) => {
+  const [matcapTexture2] = useMatcapTexture("221917_928380_5F504D_7C746C", 256);
+
+  return (
+    <mesh position={[0, -1.45, 0]}>
+      <boxGeometry args={[0.7, 0.3, 0.02]} />
+      <meshMatcapMaterial matcap={matcapTexture2} />
+    </mesh>
+  );
+};
+export default TitlePlate;
