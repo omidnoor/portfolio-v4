2f04081648e15a210e5fa92ad30292b578b40113
Omid Noorshams <o.noorshams@gmail.com>
2023-07-13 01:31:49 -0700

    fix deploying issue

--- src/pages/PageContactMe.jsx
+++
@@ -1,6 +0,0 @@
-import ContactScene from "@/components/3d/ContactMeComponents/ContactScene";
-
-const PageContactMe = () => {
-  return <ContactScene />;
-};
-export default PageContactMe;
--- src/pages/PageProjects.jsx
+++
@@ -1,6 +0,0 @@
-import Projects from "@/components/pageComponents/Projects/Projects";
-
-const PageProjects = () => {
-  return <Projects />;
-};
-export default PageProjects;
