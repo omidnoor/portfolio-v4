d7e1678c4bbdbbf492d024469ccb32ea81e3213e
Omid Noorshams <o.noorshams@gmail.com>
2023-07-13 16:46:49 -0700

    different approach for loading

--- src/components/3d/Models/SceneModel.jsx
+++ src/components/3d/Models/SceneModel.jsx
@@ -1,6 +1,7 @@
 import { useFBX, useGLTF } from "@react-three/drei";
 import { useStore } from "@/stores/store";
 import { worldScale } from "@/stores/variables";
+import { Suspense } from "react";
 
 useGLTF.preload("./models/SceneModel.glb");
 
@@ -24,9 +25,11 @@ const SceneModel = () => {
   console.log(model.nodes.ceiling);
   return (
     <mesh scale={worldScale} onClick={handleClick}>
-      <primitive object={model.nodes.ceiling} />
+      <Suspense fallback={null}>
+        <primitive object={model.nodes.ceiling} />
+        <primitive object={model.nodes.post} />
+      </Suspense>
       <primitive object={model.nodes.floor} />
-      <primitive object={model.nodes.post} />
       <primitive object={model.nodes.walls} />
     </mesh>
   );
