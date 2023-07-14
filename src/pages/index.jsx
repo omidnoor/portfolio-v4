e10a8c97d5ea89e7cfc36615b69c3db232aa8980
Omid Noorshams <o.noorshams@gmail.com>
2023-07-13 17:54:23 -0700

    removed unused components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,11 @@
 import Layout from "@/components/3d/Layout";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import CustomLoader from "@/components/utilComponents/Loader/CustomLoader";
 import SceneModel from "@/components/3d/Models/SceneModel";
-import ResponsiveCamera from "@/components/3d/Utils/ReponsiveCamera";
+
 import Navigation from "@/components/3d/UI/Navigation/Navigation";
 import ImageFrames from "@/components/3d/ImageFrames";
 import Menu from "@/components/utilComponents/UI/Menu/Menu";
-import Plate from "@/components/3d/UI/Plate/Plate";
-import Effect from "@/components/effect/Effect";
 
 const HomePage = () => {
   return (
