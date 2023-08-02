f1d97fdfff894725c27ca94716dc4c8df4428f64
Omid Noorshams <o.noorshams@gmail.com>
2023-08-02 03:54:02 -0700

    update  instruction

--- src/components/utilComponents/UI/Instruction/Instruction.js
+++ src/components/utilComponents/UI/Instruction/Instruction.js
@@ -3,11 +3,18 @@ import styles from "./styles.module.scss";
 const Instruction = () => {
   return (
     <div className={styles.instruction}>
-      <p>Navigate gallery via menu below.</p>
+      <p style={{ borderBottom: "1px solid #000", width: "70%" }}>
+        Welcome to my gallery
+      </p>
+      <br />
+      <p>Navigate via menu below.</p>
       <p>
-        Query{" "}
-        <span style={{ color: "#bda036", fontWeight: "900" }}>Chatbot</span> for
-        info about me.
+        Query
+        <span style={{ color: "#bda036", fontWeight: "900" }}>
+          {" "}
+          Chatbot
+        </span>{" "}
+        for more info.
       </p>
     </div>
   );
