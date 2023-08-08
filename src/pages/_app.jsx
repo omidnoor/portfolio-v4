925ea094f16accd0603d481b839b8c87374223b3
Omid Noorshams <o.noorshams@gmail.com>
2023-08-07 18:31:55 -0700

    add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import "../styles/variables.scss";
 import "../styles/global.scss";
 import "../styles/styles.scss";
 import Head from "next/head";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   return (
@@ -12,6 +13,19 @@ function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SE5MLR2WBR"
+      />
+      <Script>
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SE5MLR2WBR');
+  `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
